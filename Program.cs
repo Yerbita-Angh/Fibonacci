20325f08df0f9c96d7777673b182ceab33c44d98
Yerbita-Angh <alejandronicolasgarcia01@gmail.com>
2024-09-18 18:23:13 -0500

    pr10

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 0, b = 1, c;
+Console.Write("Ingrese la cantidad de la secuencia: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i=0; i<n; i++)
+{
+    Console.WriteLine(a);
+    c = a + b;
+    a = b;
+    b = c;
+}
